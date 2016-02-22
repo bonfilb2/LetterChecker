5fbaacb6ce5620831d5d6fb451a6dd7566e1fb01
bonfilb2 <brian.bonfil2@mail.dcu.ie>
2016-02-22 23:27:03 +0000

    Initial commit

---
+++ app/src/main/java/com/example/brian/letterchecker/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.example.brian.letterchecker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Brian on 22/02/2016.
+ */
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+    }
+}
