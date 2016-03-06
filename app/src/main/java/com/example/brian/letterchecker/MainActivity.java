a2cdb1bb1f1e75f75bf294aafb423e7bea562f96
Brian <brian.bonfil2@mail.dcu.ie>
2016-03-06 16:05:40 +0000

    Login and register server functionality added

--- app/src/main/java/com/example/brian/letterchecker/MainActivity.java
+++ app/src/main/java/com/example/brian/letterchecker/MainActivity.java
@@ -2,14 +2,8 @@ package com.example.brian.letterchecker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,45 +11,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        UserLocalStore userLocalStore = new UserLocalStore(this);
+
+        if(userLocalStore.isEmpty()) {
+            // if shared preferences file is empty go to login
+            startActivity(new Intent(this, Login.class));
+        }
+        // stay on main activity there's already a login saved
     }
 
     public void moveActivity(View v){
-        String button_text;
-        button_text = ((Button) v).getText().toString();
 
-        if (button_text.equals("Practice Mode"))
-        {
-            Intent intent = new Intent(this,SecondActivity.class);
-            startActivity(intent);
-        }
-        else if (button_text.equals("Quiz Mode"))
-        {
-            Intent intent = new Intent(this,ThirdActivity.class);
-            startActivity(intent);
-            //a
+        switch(v.getId()){
+            case R.id.secondActivityBtn:
+                startActivity(new Intent(this, SecondActivity.class));
+                break;
+            case R.id.thirdActivityBtn:
+                startActivity(new Intent(this, ThirdActivity.class));
+                break;
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void clearData(View v) {
+        switch(v.getId()) {
+            case R.id.clearData:
+                UserLocalStore userLocalStore = new UserLocalStore(this);
+                userLocalStore.clearUserData();
+                System.out.println("Cleared data");
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
